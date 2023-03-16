eae3b618e940e55fc54a5ec1a9af1d57d34d9ec4
Mathobela <mathobela.mawasha@umuzi.com>
2023-03-16 11:47:46 -0700

    wrote in betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-int main()
+/**
+ *main- Prints a string on stdout
+ *Return: 0 if successful
+ */
+
+int main(void)
 {
-  puts("Programming is like building a multilingual puzzle\n");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
 }
