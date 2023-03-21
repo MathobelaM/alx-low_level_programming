899bfc33db7c8cd62a237b5d823389d8e697d1fd
Mathobela <mathobela.mawasha@umuzi.com>
2023-03-21 12:50:20 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *print_alphabet- prints alphabets in lowercase
+ *from a to z
+ *Return: void
+ */
+
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	putchar('\n');
+}
