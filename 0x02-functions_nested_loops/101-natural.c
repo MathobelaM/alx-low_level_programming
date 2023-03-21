bde0bee4b613ca9a0e24a8aed57a1bb0f56d6758
Mathobela <mathobela.mawasha@umuzi.com>
2023-03-21 15:10:43 -0700

    task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *multiples- computes and prints the sum
+ *of all the multiples of 3 or 5 below 1024 (excluded)
+ *followed by a  new line
+ *Return: sum
+ */
+
+int multiples(void)
+{
+	int i;
+	int pro;
+
+	for (i = 1; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			pro += i;
+		}
+	}
+	puts(pro);
+	return (pro);
+}
