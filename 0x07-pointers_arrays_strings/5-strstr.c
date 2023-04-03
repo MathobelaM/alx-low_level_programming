a4ba533b4d400fe6ebb0f1e8ad1c3b8d29723aa1
Mathobela <mathobela.mawasha@umuzi.com>
2023-04-03 10:36:47 -0700

    task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+/**
+ * _strstr - locates a substring
+ * @haystack: pointer param
+ * @needle: subtring to search for
+ *
+ * Return: a pointer or null
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *h, *n;
+
+	while (*haystack != '\0')
+	{
+		h = haystack;
+		n = needle;
+		while (*n != '\0' && *haystack == *n)
+		{
+			haystack++;
+			n++;
+		}
+		if (!*n)
+			return (h);
+		haystack++;
+	}
+	return ('\0');
+}
